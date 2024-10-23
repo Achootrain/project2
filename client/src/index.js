a6d6fa330b65b054473c8de5d52db4d99b841688
NXTruongdk <truongnguyen31032004@gmail.com>
2024-10-23 20:18:38 +0700

    t

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Nav from "./component/Home/Nav";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  BrowserRouter,
-} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
